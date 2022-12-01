120df435ec6f4d583e5e1deac4b35ed6f0d35bba
herbertvanleeuwen <herbertvleeuwen@gmail.com>
2022-12-01 22:40:52 +0100

    install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+set -e
+
+###
+# Installation of packages, configurations, and dotfiles.
+###
+DOTFILES_LOCATION=$(pwd)
+export DOTFILES_LOCATION;
+
+###
+# Install dependencies
+###
+#./bin/dotfiles install omz
+./bin/dotfiles install zsh
+#./bin/dotfiles install brew
+#./bin/dotfiles install vscode
+#./bin/dotfiles install git
+#./bin/dotfiles install github
+#./bin/dotfiles install node
+#./bin/dotfiles install mongodb
+#./bin/dotfiles install php
+#./bin/dotfiles install starship
+#./bin/dotfiles install tmux
+#./bin/dotfiles install vim
