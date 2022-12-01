a0847f4b61dae7354728036dedf72fa4d9eaed79
herbertvanleeuwen <herbertvleeuwen@gmail.com>
2022-12-01 22:53:12 +0100

    spaceship theme install

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,4 +2,9 @@
 
 set -e
 
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+
 ln -sf "${DOTFILES_LOCATION}/zsh/.zshrc" "${HOME}/.zshrc"
