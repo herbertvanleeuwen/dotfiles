d829be2d1e4afa91a235955137a45ac01b59c8e1
Herbert van Leeuwen <30523103+herbertvanleeuwen@users.noreply.github.com>
2023-11-25 21:44:45 +0100

    fixed not loading zsh plugins

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,6 +4,8 @@ set -e
 
 ln -sf "${DOTFILES}/zsh/rc.zsh" "${HOME}/.zshrc"
 
+ln -sf "${DOTFILES}/zsh/zprofile" "${HOME}/.zprofile"
+
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "${DOTFILES}/zsh/themes/spaceship-prompt" --depth=1
 
 ln -s "${DOTFILES}/zsh/themes/spaceship-prompt/spaceship.zsh-theme" "${HOME}/.oh-my-zsh/themes/spaceship.zsh-theme"
--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -1,15 +1,14 @@
-
 source_if_exists () {
     if test -r "$1"; then
         source "$1"
     fi
 }
 
-source_if_exists $HOME/.env.sh
-source_if_exists $DOTFILES/zsh/aliases.zsh
-
 # If you come from bash you might have to change your $PATH.
 export PATH=$HOME/bin:/usr/local/bin:$PATH
+
+source_if_exists $HOME/.env.sh
+source_if_exists $DOTFILES/zsh/aliases.zsh
 
 # add ssh ids
 #ssh-add ~/.ssh/id_github
@@ -18,7 +17,6 @@ export PATH=$HOME/bin:/usr/local/bin:$PATH
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
-export PYENV_VIRTUALENV_DISABLE_PROMPT=0
 
 # Set theme and theme config
 ZSH_THEME="spaceship"
@@ -28,34 +26,27 @@ export SPACESHIP_CONFIG=$DOTFILES/zsh/spaceship.zsh
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-# Import ZSH plugins
-plugins=(docker docker-compose pyenv git macos pylint python poetry direnv)
-
+# Import ZSH plugins for the environment
 # Apple Silicon
-#if [ $(arch) = "arm64" ]; then
-#  plugins=(docker docker-compose pyenv git macos pylint python poetry)
-#else
-#  # VS Code Containers
-#  if [[ ${REMOTE_CONTAINERS} ]] ; then
-#    plugins=(docker git)
-#  else#    plugins=(docker docker-compose pyenv git macos pylint python poetry)
-#  fi
-#fi
-
+if [ $(arch) = "arm64" ]; then
+  plugins=(docker docker-compose pyenv git macos pylint python poetry direnv)
+else
+  # VS Code Containers
+  if [[ ${REMOTE_CONTAINERS} ]] ; then
+    plugins=(docker git)
+  else
+    plugins=(docker docker-compose pyenv git macos pylint python poetry direnv)
+  fi
+fi
 source $ZSH/oh-my-zsh.sh
 
 # Pyenv settings
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
-
-# if command -v pyenv 1>/dev/null 2>&1; then
-#   eval "$(pyenv init -)"
-#   eval "$(pyenv virtualenv-init -)"
-# fi
+eval "$(pyenv init -)"
 
 # hook direnv
 eval "$(direnv hook zsh)"
-
 
 # User configuration
 # Preferred editor for local and remote sessions
