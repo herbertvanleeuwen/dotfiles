14c593b11f0371b8abbf1b64ee48734d24b4f6a8
Herbert van Leeuwen <30523103+herbertvanleeuwen@users.noreply.github.com>
2023-11-26 20:46:26 +0100

    cleaned up files

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ source ./bin/generic_shell_functions
 ### 
 
 #Setup environment file
-echo $(pwd)
 DOTFILES=$(pwd -P)
-export DOTFILES;
+export DOTFILES
+
 ./bin/env_file
 
 ###
@@ -19,13 +19,3 @@ export DOTFILES;
 ###
 ./bin/dotfiles_install install zsh
 ./bin/dotfiles_install install git
-#./bin/dotfiles install omz
-#./bin/dotfiles install brew
-#./bin/dotfiles install vscode
-#./bin/dotfiles install github
-#./bin/dotfiles install node
-#./bin/dotfiles install mongodb
-#./bin/dotfiles install php
-#./bin/dotfiles install starship
-#./bin/dotfiles install tmux
-#./bin/dotfiles install vim
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,14 +3,4 @@
 alias startvenv="source ./venv/bin/activate"
 alias pi="ssh pi@192.168.1.190"
 
-# Require Virtual environment for pip command 
-export PIP_REQUIRE_VIRTUALENV=true
-
-# 
-gpip() {
-    PIP_REQUIRE_VIRTUALENV="" pip "$@"
-}
-
-gpip3() {
-    PIP_REQUIRE_VIRTUALENV="" pip3 "$@"
-}
+alias pycharm="/Applications/PyCharm.app/Contents/MacOS/pycharm"
